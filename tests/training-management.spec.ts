6881a49cf3b2b86cf7ff3fc4e8cc61ea370ebad4
DavidRiegler <111269612+DavidRiegler@users.noreply.github.com>
2025-01-31 14:59:33 +0100

    added variables to testing-management.spec.ts

--- tests/training-management.spec.ts
+++ tests/training-management.spec.ts
@@ -1,19 +1,25 @@
 import { test, expect } from '@playwright/test';
 
+// Define variables
+const BASE_URL = 'http://localhost:4000/admin';
+const ADMIN_USERNAME = 'admin';
+const ADMIN_PASSWORD = 'password';
+const today = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
+
 test('test', async ({ page }) => {
+  
   // Navigate to login page
-  await page.goto('http://localhost:4000/admin');
+  await page.goto(BASE_URL);
 
   // Log in as admin
   await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill('admin');
+  await page.getByRole('textbox', { name: 'Username' }).fill(ADMIN_USERNAME);
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('password');
+  await page.getByRole('textbox', { name: 'Password' }).fill(ADMIN_PASSWORD);
   await page.getByRole('button', { name: 'Sign in' }).click();
 
   // Create new Training
   await page.getByRole('button').first().click();
-  const today = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
   await page.getByRole('textbox', { name: 'datum' }).fill(today);  
   await page.getByRole('textbox', { name: 'startzeit' }).click();
   await page.getByRole('textbox', { name: 'startzeit' }).fill('15:00');
